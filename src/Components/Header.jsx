8efbd7c5172f9c0c9c367a3eef3c4f6ec08214fe
Julián Castrillón <juliancastrillon681@gmail.com>
2023-10-03 17:49:32 -0500

    Inicializing repository

---
+++ src/Components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className="content-header">
+            <h1 className='image-header-title'>
+                <img src="./Images/gif.png" alt="Icon gif" title='Icon-gif' id='img-header' />
+            </h1>
+            <form action="" id="form-header">
+                <label className="search-title" htmlFor='input-header'>Busca un gif</label>
+                <input type="text" id="input-header" placeholder='Escribe aquí' />
+                <button type='submit' id="search-header">Buscar</button>
+            </form>
+        </header>
+    )
+}
+
+export default Header; 
